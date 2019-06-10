97eaf3a7e7a3c0a6980638d94b8ff743584d8168
Windows 10 <Windows 10@DESKTOP-0AB2P58>
2019-06-10 11:22:27 -0600

    Clase MyConecction

---
+++ ConexionSQL/MyConnection.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.OleDb;
+using System.Data.Odbc;
+
+namespace ConexionSQL
+{
+    public enum DataProvider { SqlServer, OleDb, Odbc, None}
+    class MyConnection
+    {
+        protected DataProvider dataProvider;
+        public MyConnection(DataProvider theDataProvider)
+        {
+            dataProvider = theDataProvider;
+        }
+
+        public void Conectar()
+        {
+
+        }
+    }
+}
