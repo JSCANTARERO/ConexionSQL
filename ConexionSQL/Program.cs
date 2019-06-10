9c60bc028410ebb70a818c4fb8412e7ef6ae3865
Windows 10 <Windows 10@DESKTOP-0AB2P58>
2019-06-10 11:16:51 -0600

    Agregar archivos de proyecto.

---
+++ ConexionSQL/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConexionSQL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
